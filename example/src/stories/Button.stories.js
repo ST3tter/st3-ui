82c0c41a9fa60740e85246d6ca39896a94c51808
ST3tter <tm@oxon.ch>
2020-11-13 22:23:14 +0100

    Started implementing an animated button

--- example/src/stories/Button.stories.js
+++ example/src/stories/Button.stories.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-import { Button } from 'st3-ui'
+import { Button } from 'st3-ui';
+import 'st3-ui/dist/index.css';
 
 export default {
   title: 'Example/Button',
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' }
-  }
-}
+    backgroundColor: { control: 'color' },
+  },
+};
 
-const Template = (args) => <Button {...args} />
+const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({})
+export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button'
-}
+  label: 'Button',
+};
 
-export const Secondary = Template.bind({})
+export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button'
-}
+  label: 'Button',
+};
--- src/Buttons/Button.jsx
+++ src/Buttons/Button.jsx
@@ -1,41 +1,132 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+
+const animationSpeed = 0.1;
 
 const Button = styled.button`
-  cursor: pointer;
-  background: transparent;
-  font-size: 16px;
-  border-radius: 3px;
-  color: palevioletred;
-  border: 2px solid palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-  transition: 0.5s all ease-out;
+  position: relative;
+  display: inline-block;
+  padding: 0;
+  border: none;
+  background: none;
+  padding: 1rem 3rem;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: #2894ff;
+  background-color: #262c37;
+  margin: 1rem;
+  letter-spacing: 2px;
 
   &:hover {
-    background-color: ${(props) => props.theme.main};
-    color: white;
+    background-color: #2894ff;
+    color: #1f242d;
+    transition: ${animationSpeed * 2}s;
+    transition-delay: ${animationSpeed * 4}s;
   }
-`
+
+  & span {
+    position: absolute;
+    display: block;
+    background-color: #2894ff;
+  }
+
+  & span:nth-child(1) {
+    left: 0;
+    bottom: 0;
+    width: 1px;
+    height: 100%;
+    transform: scaleY(0);
+    transform-origin: top;
+    transition: transform ${animationSpeed}s;
+  }
+
+  &:hover span:nth-child(1) {
+    transform: scaleY(1);
+    transform-origin: bottom;
+    transition: transform ${animationSpeed}s;
+  }
+
+  & span:nth-child(2) {
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 1px;
+    transform: scaleX(0);
+    transform-origin: right;
+    transition: transform ${animationSpeed}s;
+    transition-delay: ${animationSpeed}s;
+  }
+
+  &:hover span:nth-child(2) {
+    transform: scaleX(1);
+    transform-origin: left;
+    transition: transform ${animationSpeed}s;
+    transition-delay: ${animationSpeed}s;
+  }
+
+  & span:nth-child(3) {
+    right: 0;
+    bottom: 0;
+    width: 1px;
+    height: 100%;
+    transform: scaleY(0);
+    transform-origin: bottom;
+    transition: transform ${animationSpeed}s;
+    transition-delay: ${animationSpeed * 2}s;
+  }
+
+  &:hover span:nth-child(3) {
+    transform: scaleY(1);
+    transform-origin: top;
+    transition: transform ${animationSpeed}s;
+    transition-delay: ${animationSpeed * 2}s;
+  }
+
+  & span:nth-child(4) {
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    transform: scaleX(0);
+    transform-origin: left;
+    transition: transform ${animationSpeed}s;
+    transition-delay: ${animationSpeed * 3}s;
+  }
+
+  &:hover span:nth-child(4) {
+    transform: scaleX(1);
+    transform-origin: right;
+    transition: transform ${animationSpeed}s;
+    transition-delay: ${animationSpeed * 3}s;
+  }
+`;
 
 Button.defaultProps = {
   theme: {
-    main: 'tomato'
-  }
-}
+    main: 'tomato',
+  },
+};
 
 /**
  * Simple button
  */
 export default ({ backgroundColor, label, ...props }) => {
   return (
-    <Button
-    // type="button"
-    // className={[styles.storybookButton, mode].join(' ')}
-    // style={backgroundColor && { backgroundColor }}
-    // {...props}
-    >
-      {label}
+    // <Button
+    // // type="button"
+    // // className={[styles.storybookButton, mode].join(' ')}
+    // // style={backgroundColor && { backgroundColor }}
+    // // {...props}
+    // >
+    //   {label}
+    // </Button>
+
+    <Button>
+      <span className={styles.button_border}></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Button
     </Button>
-  )
-}
+  );
+};
